7ca198997d394a0fa00363fe8bb87fd8e92c752a
Pratik Bankar <blakleaf.003@outlook.com>
2020-10-16 00:58:31 +0530

    portfolio

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,113 +1,121 @@
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".header").removeClass("header-scroll-down").addClass("header-scroll-up");
-  }else{
+  } else {
     $(".header").removeClass("header-scroll-up").addClass("header-scroll-down");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".header-profile").addClass("header-profile-scroll");
-  }else{
+  } else {
     $(".header-profile").removeClass("header-profile-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".header-text-link-container").addClass("header-text-link-scroll");
-  }else{
+  } else {
     $(".header-text-link-container").removeClass("header-text-link-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".profile").addClass("profile-scroll");
-  }else{
+  } else {
     $(".profile").removeClass("profile-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".icon-section").addClass("icons-scroll");
-  }else{
+  } else {
     $(".icon-section").removeClass("icons-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".icon-github").addClass("icon-github-scroll");
-  }else{
+  } else {
     $(".icon-github").removeClass("icon-github-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".icon-linkedin").addClass("icon-linkedin-scroll");
-  }else{
+  } else {
     $(".icon-linkedin").removeClass("icon-linkedin-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".icon-dribbble").addClass("icon-dribbble-scroll");
-  }else{
+  } else {
     $(".icon-dribbble").removeClass("icon-dribbble-scroll");
   }
 });
 
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".text-section").addClass("text-link-scroll");
-  }else{
+  } else {
     $(".text-section").removeClass("text-link-scroll");
   }
 });
 
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".intro").addClass("intro-scroll");
-  }else{
+  } else {
     $(".intro").removeClass("intro-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".name").addClass("name-scroll");
-  }else{
+  } else {
     $(".name").removeClass("name-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $(".designation").addClass("designation-scroll");
-  }else{
+  } else {
     $(".designation").removeClass("designation-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $("main").addClass("main-scroll");
-  }else{
+  } else {
     $("main").removeClass("main-scroll");
   }
 });
 
 $(window).on("scroll", () => {
-  if ($(window).scrollTop()){
+  if ($(window).scrollTop()) {
     $("footer").removeClass("footer-scroll-down").addClass("footer-scroll-up");
-  }else{
+  } else {
     $("footer").removeClass("footer-scroll-up").addClass("footer-scroll-down");
+  }
+});
+
+$(window).on("scroll", () => {
+  if ($(window).scrollTop()) {
+    $(".hire-link").addClass("hire-scroll");
+  } else {
+    $(".hire-link").removeClass("hire-scroll");
   }
 });
